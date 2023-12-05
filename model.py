82a77e5e4aed6a053e30f301b79611969c8eb84d
nadia <67629937+nadiaverdha@users.noreply.github.com>
2023-12-05 19:57:45 +0100

    updated file

--- model.py
+++ model.py
@@ -3,129 +3,135 @@ import torch.nn as nn
 from torchvision import models
 from torch.nn.utils.rnn import pack_padded_sequence
 import numpy as np
+import torch.nn.functional as F
 
 class EncoderCNN(nn.Module):
     def __init__(self):
         super(EncoderCNN,self).__init__()
-        resnet = models.resnet152(weights=models.ResNet152_Weights.DEFAULT)
-        # freezing the parameters
-        for param in resnet.parameters():
-            param.requires_grad_(False)
+        resnet = models.resnet101(weights=models.ResNet101_Weights.DEFAULT)
         modules = list(resnet.children())[:-2]
         self.resnet = nn.Sequential(*modules)
+        self.fine_tune()
+
+    def fine_tune(self,fine_tune = True):
+        for param in self.resnet.parameters():
+            param.requires_grad = False
+
+        for child in list(self.resnet.children())[5:]:
+            for param in child.parameters():
+                param.requires_grad = fine_tune
 
     def forward(self,images):
         features = self.resnet(images)
-        # the tensor image has to be of size (batch, size*size, feature_maps)
-        batch, feature_maps, size_1, size_2 = features.size()
         features = features.permute(0,2,3,1)
-        features = features.view(batch,size_1*size_2,feature_maps)
+        return features
 
 
-#additive attention as described by Bahdanau Paper
 class Attention(nn.Module):
-    def __init__(self,num_features, hidden_dim, output_dim = 1):
+    def __init__(self,encoder_dim, decoder_dim, attention_dim):
         super(Attention,self).__init__()
-        self.num_features = num_features
-        self.hidden_dim = hidden_dim
-        self.output_dim = output_dim
+        #linear layer to transform encoder's & decoder's  output
+        self.encoder_attn = nn.Linear(encoder_dim,attention_dim)
+        self.decoder_attn = nn.Linear(decoder_dim,attention_dim)
+        self.full_attn = nn.Linear(attention_dim,1)
 
-        # linear layers to learn weights for the attention mechanism
-        self.W_a = nn.Linear(self.num_features,self.hidden_dim)
-        self.U_a = nn.Linear(self.hidden_dim,self.hidden_dim)
-        self.v_a = nn.Linear(self.hidden_dim,self.output_dim)
+    def forward(self,encoder_out, decoder_hidden):
+        attn1 = self.encoder_attn(encoder_out)   # (batch_size, num_pixels, attention_dim)
+        attn2 = self.decoder_attn(decoder_hidden)  # (batch_size, attention_dim)
+        attn = self.full_attn(F.relu(attn1+attn2.unsqueeze(1))) # (batch_size, num_pixels, 1)
 
-    def forward(self,features, decoder_hidden):
-        #input:
-        # features - returned by encoder
-        # decoder_hidden - hidden state output from Decoder
-
-        #output:
-        # context - context vector with size (1,2048)
-        # attent_weight - expresses feature relevance
-
-        decoder_hidden = decoder_hidden.unsqueeze(1)
-        atten_1 = self.W_a(features)
-        atten_2 = self.U_a(decoder_hidden)
-
-        # applying tangent to combine from the two 2 fc layers
-        atten_tan = torch.tanh(atten_1+atten_2)
-        atten_score = self.v_a(atten_tan)
-        #multiplying each vector by its softmax score
-        atten_weight = F.softmax(atten_score,dim = 1)
-        # sum up the vectors - which produce the attention context
-        context = torch.sum(atten_weight* features, dim = 1)
-        atten_weight = atten_weight.squeeze(dim = 2)
-
-        return context, atten_weight
+        #softmax for calculating weights for weighted encoding based on attention
+        alpha = F.softmax(attn, dim = 1) # (batch_size, num_pixels,1)
+        attn_weighted_encoding = (encoder_out*alpha).sum(dim = 1)  # (batch_size, encoder_dim)
+        alpha = alpha = alpha.squeeze(2) # (batch_size, num_pixels)
+        return attn_weighted_encoding,alpha
 
 class DecoderRNN(nn.Module):
-    def __init__(self,num_features, embedding_dim,hidden_dim, vocab_size,p = 0.5):
+    def __init__(self,attention_dim, embed_dim,decoder_dim, vocab_size,device, encoder_dim = 2048, dropout = 0.5,):
         super(DecoderRNN,self).__init__()
-        self.num_features = num_features
-        self.embedding_dim = embedding_dim
-        self.hidden_dim = hidden_dim
+        self.attention_dim = attention_dim
+        self.embed_dim = embed_dim
+        self.decoder_dim = decoder_dim #feature size of decoder's RNN
         self.vocab_size = vocab_size
-        #for scaling the inputs to softmax
-        self.sample_temp = 0.5
+        self.device = device
+        self.encoder_dim = encoder_dim #feature size of encoded images
+        self.dropout = dropout
 
-        #turns vectors into a vector of a specified length
-        self.embeddings = nn.Embedding(vocab_size,embedding_dim)
-        self.lstm = nn.LSTMCell(embedding_dim + num_features , hidden_dim)
-        #final output
-        self.fc = nn.Linear(hidden_dim,vocab_size)
+        self.attention = Attention(encoder_dim,decoder_dim,attention_dim)
+        self.embbeding = nn.Embedding(vocab_size,embed_dim)
+        self.dropout = nn.Dropout(p = dropout)
 
-        #adding attention layer
-        self.attention = Attention(num_features,hidden_dim)
-        self.drop = nn.Dropout(p = p)
+        self.decode_step = nn.LSTMCell(embed_dim+encoder_dim,decoder_dim,bias = True)
+        self.init_h = nn.Linear(encoder_dim, decoder_dim)
+        self.init_c = nn.Linear(encoder_dim, decoder_dim)
+        self.f_beta = nn.Linear(decoder_dim, encoder_dim)
 
-        #initializing hidden state and cell memory using average feature vector
-        self.init_h = nn.Linear(num_features,hidden_dim)
-        self.init_c = nn.Linear(num_features,hidden_dim)
+        self.fc = nn.Linear(decoder_dim, vocab_size)
+        self.init_weights()
 
-    #initializes hidden state and cell memory using average feature vector
-    def init_hidden(self,features):
-        mean_annotations = torch.mean(features,dim = 1)
-        #hidden state (short-term memory)
-        h0 = self.init_h(mean_annotations)
-        #initial cell state (long-term memory)
-        c0 = self.init_c(mean_annotations)
-        return h0,c0
+    def init_weights(self):
+        #initializes layers w/ uniform distribution for easier convergence
+        self.embedding.weight.data.uniform_(-0.1,0.1)
+        self.fc.bias.data.fill_(0)
+        self.fc.weight.data.uniform_(-0.1,0.1)
 
-    def forward(self,captions, features, sample_prob = 0.0):
-        # captions - image captions ; features -  features returned by encoder
-        #sample_prob - for scheduled sampling
+    def init_hidden_state(self, encoder_out):
+        mean_encoder_out = encoder_out.mean(dim=1)
+        h = self.init_h(mean_encoder_out)
+        c = self.init_c(mean_encoder_out)
+        return h, c
+    def forward(self,encoder_out,encoded_captions,caption_lens):
 
-        embed = self.embeddings(captions)
-        h,c = self.init_hidden(features)
-        seq_len = captions.size(1)
-        feature_size = features.size(1)
-        batch_size = features.size(0)
-        outputs = torch.zeros(batch_size,seq_len,self.vocab_size).to(device)
-        atten_weights = torch.zeros(batch_size,seq_len,feature_size).to(device)
+        batch_size = encoder_out.size(0)
+
+        #flatten image
+        encoder_out = encoder_out.view(batch_size,-1,self.encoder_dim)  #encoded image
+        num_pixels = encoder_out.size(1)
+
+        # sorting  input data by the decreasing caption length
+        caption_lens, sort_idx = caption_lens.squeeze(1).sort(dim=0, descending=True)
+        encoder_out = encoder_out[sort_idx]
+        encoded_captions = encoded_captions[sort_idx]
+
+        embeddings = self.embedding(encoded_captions)
+
+        h,c = self.init_hidden_state(encoder_out)
+
+        # <end> token will not be included
+        decode_lens = (caption_lens -1).tolist()
+
+        predictions = torch.zeros(batch_size, max(decode_lens),self.vocab_size).to(self.device)
+        alphas = torch.zeros(batch_size, max(decode_lens), num_pixels).to(self.device)
+
+        for t in range(max(decode_lens)):
+            batch_size_t = sum([l>t for l in decode_lens])
+
+            #attention weighted encodings
+            attention_weighted_encoding,alpha = self.attention(encoder_out[:batch_size_t],h[:batch_size_t])
+
+            #sigmoid gating scalar
+            gate = F.sigmoid(self.f_beta(h[:batch_size_t]))
+            attention_weighted_encoding = gate * attention_weighted_encoding
+
+            h, c = self.decode_step(
+                torch.cat([embeddings[:batch_size_t, t, :], attention_weighted_encoding], dim=1),
+                (h[:batch_size_t], c[:batch_size_t]))
+
+            #next word prediction
+            preds = self.fc(self.dropout(h))
+
+            #save the prediction and alpha for every time step
+            predictions[:batch_size_t, t, :] = preds
+            alphas[:batch_size_t, t, :] = alpha
+
+        return predictions, encoded_captions, decode_lens, alphas, sort_idx
 
 
-        for t in range(seq_len):
-            sample_prob = 0.0 if t== 0 else 0.5
-            use_sampling = np.random.random() < sample_prob
 
-            if use_sampling == False:
-                word_embed = embed[:,t,:]
-            context, atten_weight = self.attention(features,h)
-            input_concat = torch.cat([word_embed,context],1)
-            h,c = self.lstm(input_concat,(h,c))
-            h = self.drop(h)
-            output = self.fc(h)
 
-            if use_sampling == True:
-                scaled_output = output /self.sample_temp
-                scoring = F.log_softmax(scaled_output,dim = 1)
-                top_idx = scoring.topk(1)[1]
-                word_embed = self.embeddings(top_idx).squeeze(1)
-            outputs[:,t,:] = output
-            atten_weights[:,t,:] = atten_weight
 
-        return outputs,atten_weights
+
+
 
 
 
