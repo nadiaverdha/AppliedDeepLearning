01578bba1d06a0cbc6fbc4d75c016403e19f6746
nadia <67629937+nadiaverdha@users.noreply.github.com>
2023-12-08 11:20:39 +0100

    modified loader

--- loader.py
+++ loader.py
@@ -35,12 +35,14 @@ def denormalize(image):
 
 
 class FlickrDataset(data.Dataset):
-    def __init__(self,captions_file, transform,vocab,images_folder,captions_per_image=5,):
+    def __init__(self,captions_file, transform,vocab,images_folder,captions_per_image=5,train_mode = True):
         self.images_folder = images_folder
         self.transform = transform
         self.captions_file = captions_file
         self.captions_per_image = captions_per_image
         self.samples = []
+        self.all_captions = {}
+        self.train_mode = train_mode
         with open(captions_file, 'r',encoding='utf-8') as file:
             for i, line in enumerate(file):
                 if i==0:
@@ -53,6 +55,10 @@ class FlickrDataset(data.Dataset):
                 word_ids = [vocab.word_to_idx(word) for word in words]
                 sample = {'image_id':image_id, 'caption':torch.LongTensor(word_ids)}
                 self.samples.append(sample)
+                if image_id not in self.all_captions:
+                    self.all_captions[image_id] = [torch.LongTensor(word_ids)]
+                else:
+                    self.all_captions[image_id].append(torch.LongTensor(word_ids))
 
     def __len__(self):
         return len(self.samples)
@@ -67,26 +73,33 @@ class FlickrDataset(data.Dataset):
             image = Image.new('RGB',(256,256))
         if self.transform:
             image = self.transform(image)
-        return image, sample['caption']
+        if self.train_mode:
+            return image, sample['caption']
+        else:
+            return image, sample['caption'],self.all_captions[sample['image_id']]
+
 
 #by introducing padding I will make sure that all my captions have the same length
 class Padding:
-    def __init__(self,pad_idx, batch_first = True):
+    def __init__(self,pad_idx, batch_first = True,train_mode = True):
         self.pad_idx = pad_idx
         self.batch_first = batch_first
-
+        self.train_mode = train_mode
     def __call__(self,batch):
         imgs = [item[0].unsqueeze(0) for item in batch]
         imgs = torch.cat(imgs,dim = 0)
 
         captions = [item[1] for item in batch]
         lengths = [len(cap) for cap in captions]
-        captions = pad_sequence(captions,batch_first=self.batch_first,padding_value=self.pad_idx)
-        return imgs,captions,lengths
 
+        captions = pad_sequence(captions,batch_first=self.batch_first,padding_value=self.pad_idx)
+        if self.train_mode:
+            return imgs,captions,lengths
+        else:
+            all_caps = [item[2] for item in batch]
+            return imgs, captions, lengths, all_caps
 def get_data_loader(dataset,batch_size = 32,pad_index = 0):
         return DataLoader(dataset=dataset, batch_size= batch_size,
-                          num_workers=3,pin_memory=True, shuffle=True,  collate_fn= Padding(pad_idx=pad_index,batch_first=True))
-
+                          num_workers=3,pin_memory=True, shuffle=True,  collate_fn= Padding(pad_idx=pad_index,batch_first=True,train_mode=True))
 
 
